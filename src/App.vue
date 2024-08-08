fc0c1f5bcd6e855a75445a3c3fa035f0be07a8f7
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-08 16:33:46 +0200

    used slot to make styling and mark up independent from the component where there is a logic

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <the-header></the-header>
     <button @click="setSelectedComponent('active-goals')">Active Goals</button>
-    <button @click="setSelectedComponent('manage-goals')">Dynamic Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
     <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
     <manage-goals v-if="selectedComponent === 'managed-goals'"></manage-goals> -->
     <keep-alive>
--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,4 +1,36 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text" />
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <error-alert v-if="inputIsInvalid">
+    <h2>Input is invalid!</h2>
+    <p>Please enter at least a few characters...</p>
+    <button @click="confirmError">Okey</button>
+  </error-alert>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredGoal = this.$refs.goal.value;
+      if (enteredGoal === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
